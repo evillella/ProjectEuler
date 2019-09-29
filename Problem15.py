435a1dec9977bb122e1c51246600afdd20f0fedc
Elise Villella <elisevillella@gmail.com>
2019-09-29 15:48:59 -0400

    completed problem 15

--- Problem15.py
+++ Problem15.py
@@ -4,3 +4,14 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to t
 are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid?
 """
+
+#for 2x2 grid, need two rights (R) and two downs (D), R,R,D,D, in any order. There are choose(4,2) options.
+
+#for 20x20 it's 20 R's and 20 D's, so we have choose(40,20)
+
+def factorial(n):
+    if n==1:
+        return n
+    else: return n*factorial(n-1)
+    
+print(factorial(40)/(factorial(20))**2)
