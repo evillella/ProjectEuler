41a8df593731e0d7a2b594a29bd6a5ad420bdd70
Elise Villella <elisevillella@gmail.com>
2019-09-30 18:28:12 -0400

    Problem 14 complete

--- Problem14.py
+++ Problem14.py
@@ -10,3 +10,33 @@ not been proved yet (Collatz Problem), it is thought that all starting numbers f
 Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
+
+def next_term(n):
+    if n%2==0:
+        return n/2
+    else:
+        return 3*n+1
+    
+length_dict = {1:1}    
+    
+def get_length(n):
+    if n==1:
+        return 1
+    else:
+        next = next_term(n)
+        if next in length_dict.keys():
+            return length_dict[next]+1
+        else:
+            return 1+get_length(next)
+    
+longest_chain_start = 1
+chain_length = 1
+for i in range(1,1000000):
+    len = get_length(i)
+    length_dict[i] = len
+    if len > chain_length:
+        chain_length = len
+        longest_chain_start = i
+        
+print(longest_chain_start)
+    
