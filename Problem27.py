cf8e9d8599eb70228719de9b97c85b6faa356b52
Elise Villella <elisevillella@gmail.com>
2020-07-01 20:05:28 -0400

    completed

--- Problem27.py
+++ Problem27.py
@@ -15,3 +15,66 @@ where |n| is the modulus/absolute value of n
 e.g. |11|=11 and |−4|=4
 Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
 """
+
+primes = {2,3,5,7}
+max_checked = 7
+
+def is_prime(n):
+    global max_checked
+    if n <= max_checked:
+        return n in primes
+    else:
+        while n > max_checked:
+            max_checked += 1
+            # figure out whether max_prime
+            is_prime = True
+            for p in primes:
+                d = int(max_checked/p)
+                if d*p == max_checked:
+                    is_prime = False
+            if is_prime:
+                primes.add(max_checked)
+        return n in primes
+    
+def num_primes(a,b):
+    n = 0
+    while is_prime(n*(n+a)+b):
+        n+=1
+    return n-1
+
+import math
+
+def show_progress_bar(bar_length, completed, total):
+    bar_length_unit_value = (total / bar_length)
+    completed_bar_part = math.ceil(completed / bar_length_unit_value)
+    progress = "*" * completed_bar_part
+    remaining = " " * (bar_length - completed_bar_part)
+    percent_done = "%.2f" % ((completed / total) * 100)
+    print(f'[{progress}{remaining}] {percent_done}%', end='\r')
+
+bar_length = 30
+total = 1999
+for i in range(0, total + 1):
+    show_progress_bar(bar_length, i, total)
+
+def solution():
+    max_n = 0
+    prod_ab = 0
+    for a in range(-999,1000,1):
+        for b in range(-1000,1001,1):
+            show_progress_bar(bar_length, a+999, total)
+            n = num_primes(a,b)
+            if n > max_n:
+                print(n,a,b)
+                max_n = n
+                prod_ab = a*b
+    return prod_ab
+            
+            
+if __name__ == "__main__":
+    print(is_prime(25))
+    print(is_prime(37))
+    print(primes)
+    
+    print(solution())
+            
